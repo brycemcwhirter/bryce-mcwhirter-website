c68c9632da847ac784752eb6891d17b13e0f37e3
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-08-14 06:35:08 -0500

    Default Styling and Working on Layout

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,9 +1,24 @@
 <script>
+  import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
   import Box from './Box.svelte';
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+  import {
+    faEnvelope,
+    faMailReply,
+    faVoicemail,
+  } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<div class=" bg-blue-300 min-h-screen grid grid-cols-2">
-  <div class="">Bio</div>
+<div class=" min-h-screen grid grid-cols-2">
+  <div class="flex justify-center items-center">
+    <p class="text-center p-5 text-lg">
+      My name is Bryce McWhirter and I'm a full stack developer based in the DFW
+      area. I'm a graduate from Baylor University with a Bachelor of Science in
+      Computer Science and a minor in Entrepreneurship and Corporate Innovation.
+      I enjoy web development, reading, theology, and annoying my friends about
+      all things music.
+    </p>
+  </div>
   <div class="grid grid-rows-3">
     <div class="border">Blog</div>
     <div class="border">Resumé</div>
@@ -24,11 +39,30 @@
   <Box title="Title C" />
 </div>
 
-<div class="border min-h-28 flex flex-col justify-center items-center">
+<div class="min-h-28 flex flex-col justify-center items-center">
   <h1>Contact</h1>
   <div class="flex flex-row space-x-3">
-    <h2>linkedin icon</h2>
-    <h2>github icon</h2>
-    <h2>gmail icon</h2>
+    <a
+      href="https://www.linkedin.com/in/bryce-mcwhirter-5b9330146/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon="{faLinkedin}" />
+    </a>
+    <a
+      href="https://github.com/brycemcwhirter"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i class="fa-brands fa-github"></i>
+      <FontAwesomeIcon icon="{faGithub}" />
+    </a>
+    <a
+      href="mailto:mcwhirterbryce@gmail.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon="{faEnvelope}" />
+    </a>
   </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+
+    },
+    colors:{
+      'primaryBlue': '#013D5A',
+      'primaryWhite': '#FCF3E3',
+      'secondaryBlue': '#BDD3CE',
+      'secondaryGreen': '#708c69',
+      'secondaryOrange':'#F4A258'
+    }
   },
 
   plugins: [],
