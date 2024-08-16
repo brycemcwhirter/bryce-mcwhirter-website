f7ca8fbdd4f3462f9247b21147d64972bb82f07b
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-08-16 06:23:04 -0500

    Box Styling.

--- src/components/Boxes.svelte
+++ src/components/Boxes.svelte
@@ -1,8 +1,6 @@
 <script>
-  import Photo from './boxes/Photo.svelte';
   import boxes from '../data/boxes.json';
-  import Project from './boxes/Project.svelte';
-  import Post from './boxes/Post.svelte';
+
   const PROJECT = 'project';
   const PHOTO = 'photo';
   const POST = 'post';
@@ -10,12 +8,31 @@
 
 {#each boxes as box}
   <div class="border min-h-[50vh]">
+    <!-- Style for a Photo Box -->
     {#if box.type == PHOTO}
-      <Photo box />
+      <div class="p-10 flex flex-col justify-center items-center">
+        <img src="{box.imageLink}" alt="{box.alt}" />
+        <h1>{box.title}</h1>
+      </div>
+
+      <!-- Style for a post -->
     {:else if box.type == POST}
-      <Post box />
+      <div class="flex flex-col items-center justify-center h-full">
+        <h1>{box.title}</h1>
+        <h4>{box.headline}</h4>
+      </div>
+
+      <!-- Style for a Project -->
     {:else if box.type == PROJECT}
-      <Project box />
+      <div class="p-6">
+        <img src="{box.imageLink}" alt="{box.altTag}" class="p-10" />
+        <h1>{box.title}</h1>
+        <p>{box.description}</p>
+        {#each box.languages as language}
+          <!-- TODO Font Awesome Logos Here -->
+          <p>{language}</p>
+        {/each}
+      </div>
     {/if}
   </div>
 {/each}
--- src/components/boxes/Post.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  export let box;
-</script>
-
-<h1>{box.title}</h1>
