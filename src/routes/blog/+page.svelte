5a88d9cb1a2b7fe808e077cdeb1c2353fcc406b0
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-08-21 05:31:14 -0500

    Implementing Blog.

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import posts from '/src/data/posts.json';
+</script>
+
+<div class="p-8">
+  {#each posts as post}
+    <div class="mb-8">
+      <div class="text-2xl font-bold">{post.title}</div>
+      <div class="text-base">{post.headline}</div>
+    </div>
+  {/each}
+</div>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<main>
+  <h1 class="text-2xl font-bold">Title</h1>
+  <p class="mt-4">Content goes here</p>
+</main>
+
+<style>
+  /* Add any custom styles here */
+</style>
