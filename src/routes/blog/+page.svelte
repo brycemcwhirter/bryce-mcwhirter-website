88b8adf3c8a9a37c49145609bf5c2fce7566cb3f
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-08-26 06:51:10 -0500

    Continuing Work on Blog.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div class="grid grid-flow-row grid-rows-2">
-    <div class="grid grid-rows-2">
+    <div class="grid grid-rows-2 p-10">
         <h1 class="p-10 text-3xl font-bold flex justify-center align-center">Subscribe Here For Updates</h1>
         <div class="p-5 flex justify-center align-center">
-         <input type="email" class="p-4" placeholder="Email Address" />
-         <button class="p-4 bg-primaryBlue text-white">Subscribe</button>
+         <input type="email" class="p-4 m-4 border border-r-primaryWhite" placeholder="Email Address" />
+         <button class="p-4 m-4 bg-primaryBlue border text-white">Subscribe</button>
         </div>
     </div>
 
